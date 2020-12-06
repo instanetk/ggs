e7e2a311eb46bba18f52c3942432aabb665ced7f
David Quintero <davidq@gmail.com>
2020-12-05 22:10:26 -0500

    Initial commit

--- src/App.js
+++ src/App.js
@@ -6,17 +6,8 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h2>General Global Services</h2>
+        <h3>Single-Page-Application</h3>
       </header>
     </div>
   );
