71805cb3340edfd0a55a4bb6f439e8f272666394
David Quintero <davidq@gmail.com>
2020-12-11 16:21:02 -0500

    register schedule, service, chat and login routes

--- src/App.js
+++ src/App.js
@@ -3,7 +3,10 @@ import { Route, Redirect, Switch } from 'react-router-dom';
 import detectBrowserLanguage from 'detect-browser-language';
 import NavBar from './components/common/navBar.jsx';
 import Home from './components/home';
-import Login from './components/login/login';
+import Schedule from './components/schedule';
+import Services from './components/services';
+import Chat from './components/chat';
+import Login from './components/login';
 import NotFound from './components/notFound';
 
 const App = () => {
@@ -15,6 +18,9 @@ const App = () => {
     <div id="App" className="min-h-screen antialiasing bg-gray-100 sm:flex">
       <NavBar />
       <Switch>
+        <Route path="/schedule" component={Schedule} />
+        <Route path="/services" component={Services} />
+        <Route path="/chat" component={Chat} />
         <Route path="/login" component={Login} />
         <Route path="/not-found" component={NotFound} />
         <Route path="/" exact render={(props) => <Home {...props} lang={lang} />} />
---
+++ src/components/services.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Services = () => {
+  return <h1>Services</h1>;
+};
+
+export default Services;
