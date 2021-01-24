e080eeaf9cadb7766d3164fe72f722ead29b8f9c
David Quintero <davidq@gmail.com>
2021-01-24 14:53:40 -0500

    change config.json into .env

--- src/services/authService.js
+++ src/services/authService.js
@@ -1,6 +1,8 @@
 import http from './httpService';
-import { apiUrl } from '../config.json';
 import jwtDecode from 'jwt-decode';
+// import { apiUrl } from '../config.json';
+
+const apiUrl = process.env.REACT_APP_API_URL;
 
 const apiEndpoint = apiUrl + '/auth';
 const tokenKey = 'token';
--- src/services/categoryService.js
+++ src/services/categoryService.js
@@ -1,5 +1,7 @@
 import http from './httpService';
-import { apiUrl } from '../config.json';
+// import { apiUrl } from '../config.json';
+
+const apiUrl = process.env.REACT_APP_API_URL;
 
 const apiEndPoint = apiUrl + '/categories';
 
