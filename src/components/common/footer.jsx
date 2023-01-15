abe1ed7e0f8e98366548e43bd9a80b5ec0735165
David Quintero <davidq@gmail.com>
2023-01-14 19:48:33 -0500

    Localization and changes to contact info

--- src/components/common/footer.jsx
+++ src/components/common/footer.jsx
@@ -8,17 +8,14 @@ const Footer = () => {
     <footer className="flex flex-col justify-center text-gray-100 bg-gray-800 sm:py-5 bgfloor">
       <div className="sm:flex">
         <div className=" sm:mx-36 mx-4 mt-8 sm:mt-8 rounded-lg text-md px-4 sm:px-20 py-6 text-justify bg-gray-900 bg-opacity-70 select-none">
-          <b>Hours of operation:</b> <br />
-          <span>Mon - Fri 8:00am to 5:00pm</span>
+          <b>{t('contact.hoursOfOperation')}</b> <br />
+          <span>{t('contact.schedule')}</span>
           <br /> <br />
-          <b>Saturday & Sunday</b>
+          <b>{t('contact.saturdayAndSunday')}</b>
           <br />
-          <span className="text-justify text-sm">
-            You may still call or text and leave a detailed message. If it's an emergency, we may get back to you right
-            away. If not, then we will contact you on the next business day.
-          </span>
+          <span className="text-justify text-sm">{t('contact.message')}</span>
           <div className="text-xl mt-4 text-center">
-            <span>407-801-3447</span>
+            <span>407-930-3447</span>
             <br />
             <span>
               <a href="mailto:main@generalglobalservices.com?subject=GGS Inquiry">main@generalglobalservices.com</a>
@@ -42,22 +39,19 @@ const Footer = () => {
               {t('footer.providers')}
             </a>
           </div>
-          <div className="text-xs text-center select-none">
+          <div className=" text-xs text-center select-none">
             <span className="hidden">
               HomeWorks<sup>&trade;</sup> is a registered trademark.
             </span>
-            {t('footer.company')} &bull; &copy; {new Date(Date.now()).getFullYear()}
-          </div>
-          <div className="text-xs text-center select-none"> {t('footer.made')}</div>
-          <div className="text-xs text-center opacity-20 select-none justify-center self-center">
-            <a href="http://wavefunction.ai" target="_blank" rel="noreferrer">
-              <span className=" italic font-serif mr-1">(x)</span>
-              <span className="">wavefunction</span>
-            </a>
-            <span className="hidden flex-col text-xs text-white bg-black opacity-20 rounded-sm px-1" title="app by">
-              app by
+            <span>
+              {t('footer.company')} &bull; &copy; {new Date(Date.now()).getFullYear()}
+            </span>
+            <span className="flex flex-col py-2">
+              <span> {t('footer.addressStreet')}</span>
+              <span> {t('footer.cityAndState')}</span>
             </span>
           </div>
+          <div className="text-xs text-center select-none"> {t('footer.made')}</div>
         </div>
       </div>
     </footer>
